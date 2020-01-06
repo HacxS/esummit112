2779090b7f4e81dff9c8ca52dc28d4fb85919eee
Divish1032 <kumar.divansh1996@gmail.com>
2020-01-07 00:38:21 +0530

    F

--- routes/index.js
+++ routes/index.js
@@ -165,7 +165,7 @@ router.post('/payment', (req, res) => {
                 }
                 else{
                   data.purpose = "Test";            // REQUIRED
-                  data.amount = 6;
+                  data.amount = total_amount;
                   data.currency                = 'INR';
                   data.buyer_name              = req.user.first_name;
                   data.email                   = req.user.email;
